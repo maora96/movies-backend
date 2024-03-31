b8ddc92bc1bff5212fab5ea256baadfd16730241
maora <marinaoliveira.r07@gmail.com>
2024-03-30 21:17:38 -0300

    feat: database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,38 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './infra/controllers/user/user.controller';
 import { UserService } from './application/services/user/user.service';
+import { MovieController } from './infra/controllers/movie/movie.controller';
+import { MovieService } from './application/services/movie/movie.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserSchema } from './infra/database/schemas/User';
+import { MovieSchema } from './infra/database/schemas/Movie';
+import { TerminusModule } from '@nestjs/terminus';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [UserSchema, MovieSchema],
+      synchronize: true,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST.toString(),
+      database: process.env.DB_DATABASE,
+      port: Number(process.env.DB_PORT),
+    }),
+    TerminusModule,
+    TypeOrmModule.forFeature([UserSchema, UserSchema]),
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  controllers: [AppController, UserController, MovieController],
+  providers: [AppService, UserService, MovieService],
+  exports: [TypeOrmModule],
 })
 export class AppModule {}
